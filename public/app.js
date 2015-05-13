c9158fb3cadb1c0d405a09a7fdcf65f06191abb3
AviFoxi <frosenox@gmail.com>
2015-05-13 11:00:48 -0400

    adding hard coded placeholders for UI

---
+++ public/app.js
@@ -0,0 +1,25 @@
+function User(name){
+	var self = this;
+	self.name = name;
+	self.schedulableBlocks = 4;
+}
+
+var eventsMay = [
+	{
+      title: 'morning rehearsal',
+      start: '2015-05-21 07:00',
+      end: '2015-05-21 09:00'
+      // ,
+      // performance: false
+  },
+  {
+      title: 'evening rehearsal',
+      start: '2015-05-22 17:00',
+      end: '2015-05-22 19:00'
+  },
+  {
+      title: 'midday rehearsal',
+      start: '2015-05-23 13:00',
+      end: '2015-05-23 15:00'
+  }
+]
